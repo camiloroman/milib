2bbf24a1476ee53997da15731a09ed913c938674
esneyder12345 <esneymoc@hotmail.com>
2015-08-13 12:37:40 -0500

    se incluyeron los codigos de las funciones de operacion calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,13 @@
+#include "calculadora.h"
+#include <stdio.h>
+int suma(int a,int b)
+return a+b;
+
+int resta(int a,int b)
+return a-b;
+
+int multiplicacion(int a,int b)
+return a*b;
+
+unt division(int a,int b)
+return a/b;

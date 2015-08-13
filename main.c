c4ffc2f0a79cb0dd1741dad3f9df9a6548126fac
esneyder12345 <esneymoc@hotmail.com>
2015-08-13 12:58:03 -0500

    se organizaron los codigos en switch case para darle opciones de operacion al usuario en main.c

--- main.c
+++ main.c
@@ -2,5 +2,28 @@
 #include"calculadora.h"
 int main(void)
 {
+int a,b,i,r;
+printf("Ingrese las 2 variables");
+scanf("%d",&a);
+scanf("%d",&b);
+printf("Ingrese:\n1 para sumar\n2 para resta\n3 para multiplicacion\n4 para division");
+scanf("%d",&i);
+
+switch(i)
+{
+case 1: 
+       r=suma(a,b);
+       break;
+case 2: 
+       r=resta(a,b);
+       break;
+case 3: 
+       r=multiplicacion(a,b);
+       break;
+case 4: 
+       r=division(a,b);
+       break;
+}
+printf("El resultado es %d",r);
 return 0;
 }
